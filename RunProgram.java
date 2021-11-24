2afa4e0125e5e26182e1ac63d5c1eab81ff455eb
DenisHristov <48285397+DenisHristov12@users.noreply.github.com>
2021-11-24 20:09:02 +0200

    Add files via upload

--- RunProgram.java
+++ RunProgram.java
@@ -27,11 +27,22 @@ public class RunProgram {
         myStore.addFishes(fishes);
 
 
-        myStore.SupplyAnimals(0);
-        while(myStore.quantity > 20) {
-            myStore.Sell(0);
-            if(myStore.quantity < 20){
-                myStore.ReSupply(0);
+        double startWorkTime = 8.00;
+        double endWorkTime = 16.00;
+
+        System.out.println("Input time of the day: ");
+        double inputTime = scan.nextDouble();
+
+
+        if(inputTime < 8 || inputTime > 16){
+            System.out.println("The store is not working!");
+        }else{
+            myStore.SupplyAnimals(0);
+            while(myStore.quantity > 20) {
+                myStore.Sell(0);
+                if(myStore.quantity < 20){
+                        myStore.ReSupply(0);
+                }
             }
         }
     }
